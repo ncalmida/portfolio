c3d4e27e7b9e093b8b51a3e11f35c45d59d87ac2
ncalmida <nc.almida@gmail.com>
2025-03-11 21:22:37 +0100

    Changes pt 4

--- js/script.js
+++ js/script.js
@@ -2,22 +2,56 @@ document.addEventListener("DOMContentLoaded", () => {
     const track = document.querySelector(".slider__track");
     const prevBtn = document.querySelector(".slider__btn--prev");
     const nextBtn = document.querySelector(".slider__btn--next");
+    const items = document.querySelectorAll(".slider__item");
 
     let index = 0;
-    const totalItems = document.querySelectorAll(".slider__item").length;
-    const itemWidth = document.querySelector(".slider__item").offsetWidth + 20;
+    const totalItems = items.length;
+    
+    function updateSliderPosition() {
+        const itemWidth = items[0].offsetWidth + 20;
+        track.style.transform = `translateX(-${index * itemWidth}px)`;
+    }
     
     nextBtn.addEventListener("click", () => {
         if (index < totalItems - 1) {
             index++;
-            track.style.transform = `translateX(-${index * itemWidth}px)`;
+            updateSliderPosition();
         }
     });
 
     prevBtn.addEventListener("click", () => {
         if (index > 0) {
             index--;
-            track.style.transform = `translateX(-${index * itemWidth}px)`;
+            updateSliderPosition();
         }
     });
+
+    window.addEventListener("resize", updateSliderPosition);
+
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const servicio = document.querySelectorAll(".servicio");
+    const modal = document.querySelector(".modal");
+    const modalTexto = document.querySelector(".modal__text");
+    const modalClose = document.querySelector(".modal__close");
+
+    servicio.forEach(servicio => {
+        servicio.addEventListener("click", function () {
+            const descripcion = this.getAttribute("data-desc");
+            modalTexto.textContent = descripcion;
+            modal.style.display = "block";
+        })
+    })
+
+    modalClose.addEventListener("click", () => {
+        modal.style.display = "none";
+    })
+
+    window.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    })
+    
+})
